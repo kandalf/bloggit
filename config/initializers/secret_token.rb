5626547c5eeb068fb24195a4760e536e303190fa
Leonardo Mateo <lmateo@atobiz.com>
2010-08-11 16:00:22 -0300

    Initial layout

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloggit::Application.config.secret_token = 'a350faa70c91505e52ca91ae94a59e138d19f7006d7ed7aace4d9267951e72914ccde9e72957bcf0bd847129f45e7f2a1fe46dc3e338b73776a03f42197783b0'
