e04696dbcd99fb2880b89bcf21e78cebd531512a
Leonardo Mateo <lmateo@atobiz.com>
2010-08-11 16:35:42 -0300

    Home page setup

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ Bloggit::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "Home#index"
 
   # See how all your routes lay out with "rake routes"
 
