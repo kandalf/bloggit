9cb1c840eab92644341155cc7481d74e74f32312
Leonardo Mateo <leonardomateo@gmail.com>
2010-08-16 16:05:34 -0300

    Validations added to Post model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe Post do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should not be valid without a name" do
+    post = Post.new(:title => "title", :content => "content")
+    post.should_not be_valid
+  end
+
+  it "should not be valid with a title smaller than 5 chars" do
+    post = Post.new(:title => "titl", :name => "Some post")
+    post.should_not be_valid
+  end
+
+  it "should be valid with a valid title and a name" do
+    post = Post.new(:title => "Valid title", :name => "valid_title")
+    post.should be_valid
+    post.save.should be_true
+  end
 end
