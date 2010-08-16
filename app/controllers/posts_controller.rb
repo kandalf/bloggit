d71e1559e5ca05175d96acf37aea80cb2221a086
Leonardo Mateo <leonardomateo@gmail.com>
2010-08-16 04:56:21 -0300

    Flash message now with I18n on create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -44,7 +44,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to(@post, :notice => 'Post was successfully created.') }
+        format.html { redirect_to(@post, :notice => t('application.messages.post_created_ok')) }
         format.xml  { render :xml => @post, :status => :created, :location => @post }
       else
         format.html { render :action => "new" }
